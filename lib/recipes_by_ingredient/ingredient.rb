023c381974e968934d5176b0a33365c2995fb81b
Juan Martin Restrepo <tinto922@gmail.com>
2020-04-14 15:25:59 -0400

    Create and Ingredient Class

--- lib/recipes_by_ingredient/ingredient.rb
+++ lib/recipes_by_ingredient/ingredient.rb
@@ -1,3 +1,21 @@
 class Ingredient
-    
+    attr_accessor :name, :meals 
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @meals = [] #ingredient has many meals 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    # this gives the ability to do find an ingredient object and look through is drinks
+    # takes in a ingredient string and returns an object with mehtods that can be call on.  
+    def find_by_ingredient(ingredient)
+        @@all.find{ |i| i.name == ingredient} 
+    end 
 end 
