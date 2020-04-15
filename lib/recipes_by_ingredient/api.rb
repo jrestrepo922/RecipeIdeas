852d42b50db7809eabce0ef126d5cc027fbd7062
Juan Martin Restrepo <tinto922@gmail.com>
2020-04-15 10:34:28 -0400

    added additional functionality to class method get_meal_details inside the Api class

--- lib/recipes_by_ingredient/api.rb
+++ lib/recipes_by_ingredient/api.rb
@@ -21,7 +21,23 @@ class Api
         url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=#{meal.meal_id}"
         response = Net::HTTP.get(URI(url)) 
 
-        meal_details = JSON.parse(response)["meals"] 
-        binding.pry 
+        meal_details = JSON.parse(response)["meals"][0]
+
+        # what do we want to do with the meal details? 
+            # We want to add the addional information to the choosen meal. 
+            # we can iterate over the keys array 
+        meal_details.keys.each do |key|
+            # will be shoveling into the meal.ingredients (instance variable that is an empty array)
+            # what will be shoveling is the values of the the keys with the name ingredient in them and that are not am empty string
+            meal.ingredients << meal_details[key] if (key.include?("Ingredient")) && (meal_details[key] != "" || meal_details[key] != " " || meal_details[key] != nil  )
+
+            meal.measures << meal_details[key] if (key.include?("Measure")) && (meal_details[key] != "" || meal_details[key] != " " || meal_details[key] != nil  )
+
+                
+        end 
+    binding.pry     
     end 
+
+
+
 end 
